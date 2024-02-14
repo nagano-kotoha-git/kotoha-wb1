e9e989c222c01c6fb22497e0f49f0dfcc1d8f7ca
nagano-kotoha-git <106937769+nagano-kotoha-git@users.noreply.github.com>
2024-02-14 10:46:04 +0900

    Add files via upload

---
+++ js/event.js
@@ -0,0 +1,201 @@
+// メニューをクリックしたら
+$("#openbtn").click(function () {
+    $(this).toggleClass('active');
+    $("#g-nav").toggleClass('panelactive');
+    // bodyにスクロールさせないclassを追加
+    $("body").toggleClass("active");
+});
+
+$("#g-nav a").click(function () {
+    $("#openbtn").removeClass('active');
+    $("#g-nav").removeClass('panelactive');
+});
+
+
+//スクロールした際の動きを関数でまとめる
+function PageTopAnime() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {//上から200pxスクロールしたら
+        $('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
+        $('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
+    } else {
+        if ($('#page-top').hasClass('UpMove')) {//すでに#page-topにUpMoveというクラス名がついていたら
+            $('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
+            $('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
+        }
+    }
+    if (scroll >= 500) {
+        $('#openbtn').addClass('UpMove');
+    } else {
+        if ($('#openbtn').hasClass('UpMove')) {
+            $('#openbtn').removeClass('UpMove');
+        }
+    }
+}
+
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+    PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+    PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// #page-topをクリックした際の設定
+$('#page-top a').click(function () {
+    $('body,html').animate({
+        scrollTop: 0//ページトップまでスクロール
+    }, 500);//ページトップスクロールの速さ。数字が大きいほど遅くなる
+    return false;//リンク自体の無効化
+});
+
+var windowSize = $(window).width();
+var slidesToShow = 1.09;
+var dots = true;
+if (windowSize >= 768) {
+    slidesToShow = 3;
+    dots = false;
+};
+
+function delayScrollAnime() {
+	var time = 0.2;//遅延時間を増やす秒数の値
+	var value = time;
+	$('#renewal').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+	$('#policy').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+	$('#education').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+	$('.slick-track').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+		
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+				
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+					
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+					
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}
+				}
+			})
+		}else {
+			$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+			value = time;//delay初期値の数値に戻す
+		}
+	})
+};
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function (){
+    delayScrollAnime();/* アニメーション用の関数を呼ぶ*/
+});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+
+
+
+$(".blog-slider").slick({
+    autoplay: false,
+    slidesToShow: slidesToShow, // 表示するスライド枚数
+    slidesToScroll: 1,// スライドする数
+    infinite: false,
+    row: 1,
+    dots: dots,
+    arrows: false,
+    dotsClass: "slide-dots",
+    // prevArrow: '<div class="slick-prev"></div>',
+    // nextArrow: '<div class="slick-next"></div>',
+})
